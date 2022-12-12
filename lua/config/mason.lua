67e6528be3b54a7b40481f949499700bbd6ba157
Darius <dariuscorvus@pm.me>
2022-12-12 08:33:20 +0100

    feat: passed on_attach to handler

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -1,10 +1,11 @@
 require("mason").setup()
 local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require("lspconfig")
+local lsp = require("config/lsp")
 
 mason_lspconfig.setup()
 mason_lspconfig.setup_handlers({
 	function(server_name)
-		lspconfig[server_name].setup({})
+		lspconfig[server_name].setup({ on_attach = lsp.on_attach })
 	end,
 })
