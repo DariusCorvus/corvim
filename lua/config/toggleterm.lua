3d0ad366ac6f0ee1b7f94c1f57abec1a0aca8976
Darius <dariuscorvus@pm.me>
2022-12-13 10:17:32 +0100

    feat: added config for terminal size

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -6,7 +6,15 @@ end
 
 local M = {
 	terminals = {},
-	config = {},
+	config = {
+		size = function(term)
+			if term.direction == "horizontal" then
+				return vim.fn.winheight(0) * 0.4
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.4
+			end
+		end,
+	},
 }
 
 toggleterm.setup(M.config)
