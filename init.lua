3d0bdecb3f0455077ef89da7785d09aa573c1d5e
DariusCorvus <dariuscorvus@pm.me>
2023-03-23 10:34:04 +0100

    feat: new indent character

--- init.lua
+++ init.lua
@@ -309,7 +309,7 @@ require("Comment").setup()
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 require("indent_blankline").setup({
-	char = "⁞",
+	char = "⎸",
 	show_trailing_blankline_indent = false,
 })
 
