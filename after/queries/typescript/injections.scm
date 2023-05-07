ca6c6ff749fdaedd2bb0ed2a7724726fc636224b
Darius <dariuscorvus@pm.me>
2023-05-07 15:16:17 +0200

    chore(git): ignore dir `after/queries` and `queries/surrealdb`

--- after/queries/typescript/injections.scm
+++
@@ -1,4 +0,0 @@
-; extends
-((template_string) @sql (#match? @sql "^`\n*( )*-{2,}( )*[sS][qQ][lL]( )*\n"))
-
-(((comment) @_comment (#match? @_comment "sql") (lexical_declaration(variable_declarator[(string(string_fragment)@sql)(template_string)@sql]))) @sql)
