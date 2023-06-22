6cf60de6a47b8e4f50bcb225c17499b760513e0b
Darius <dariuscorvus@pm.me>
2023-06-22 20:44:00 +0200

    style(foldcolumn): disabled

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -38,7 +38,7 @@ local function configure(_)
   vim.o.smarttab = true
 
   -- Folding
-  vim.o.foldcolumn = "1"
+  vim.o.foldcolumn = "0"
   vim.o.foldlevel = 99
   vim.o.foldlevelstart = 99
   vim.o.foldenable = true
