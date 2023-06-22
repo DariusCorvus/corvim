e14d9f41dec841b8a3fcd6aa01560cd1f7fdd466
Darius <dariuscorvus@pm.me>
2023-06-22 14:50:49 +0200

    style(gitsigns): updated signs

--- init.lua
+++ init.lua
@@ -105,11 +105,11 @@ require('lazy').setup({
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = { text = '╎' },
+        change = { text = '╎' },
+        delete = { text = '╷' },
+        topdelete = { text = '╵' },
+        changedelete = { text = '╎' },
       },
     },
   },
