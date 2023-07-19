14968f1df04a478519430d14eef596e8a6c47975
Darius <dariuscorvus@pm.me>
2023-07-19 11:12:04 +0200

    feat(defaults): default shell `pwsh` if `os` is `Windows`

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,9 +1,12 @@
 local function configure(_)
-  -- shell
-  vim.o.shell = 'pwsh'
-  vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues.Add('Out-File:Encoding', 'utf8');"
-  vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-  vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+  -- Windows Defaults
+  if jit.os == 'Windows' then 
+    -- shell
+    vim.o.shell = 'pwsh'
+    vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues.Add('Out-File:Encoding', 'utf8');"
+    vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+  end
 
   -- Set title to file name and vim icon
   vim.o.title = true
